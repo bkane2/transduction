453aa07aefc9a12c89007fa6bf67cdf18b3f552b
bkane2 <Ben.kane@me.com>
2023-09-07 23:28:15 -0400

    Fixed bug with custom template preds

--- src/transduction/tt.py
+++ src/transduction/tt.py
@@ -512,6 +512,7 @@ def fill_template(template, match_result, preds={}):
       else:
         return cons(flatten_sequences(val), fill_template_rec(template[1:], match_result))
     if spec_function(template[0]):
+      fname = template[0][:-1]
       if fname in DEFAULT_PREDS:
         f = DEFAULT_PREDS[fname]
       elif fname in preds:
